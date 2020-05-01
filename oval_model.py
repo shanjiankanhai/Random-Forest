d83f6de311c3a4053961366563baded271c42980
shanjiankanhai <shanjiankanhai@163.com>
2020-05-01 13:07:11 +0800

    changed

--- oval_model.py
+++ oval_model.py
@@ -13,69 +13,50 @@ import time
 root = tk.Tk()
 root.withdraw()
 
-file_path = filedialog.askopenfilename()
-img_read = cv2.imread(file_path)
+file_path = filedialog.askopenfilename()   # 读取YCrCb空间的图像
+# Cr_csv_path = filedialog.askopenfilename()
+# Cb_csv_path = filedialog.askopenfilename()
+# img_csv_path = filedialog.askopenfilename()
+face_out_path = filedialog.askopenfilename()
+dir_path = filedialog.askdirectory()
+oval_name = dir_path + r'/oval.png'
 
-'''
-d = 400
-img = np.ones((d, d, 3), dtype='uint8')*255
-center = (round(d/2), round(d/2))
-size = (100, 200)
-for i in range(0, 10):
-    angle = np.random.randint(0, 361)
-    color = np.random.randint(0, high=256, size=(3, )).tolist()
-    thickness = np.random.randint(1, 9)
-    cv2.ellipse(img_read, center, size, angle, 0, 360, color, thickness)
-cv2.imshow('demo', img_read)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-'''
+img_read = cv2.imread(file_path)   # 读取图像内容
+img_YCrCb = cv2.cvtColor(img_read, cv2.COLOR_RGB2YCrCb)   # 转换色彩空间
 
+# 构建椭圆肤色模型
 oval_matrix = np.zeros((256, 256), dtype=np.uint8)   # 建立一个用于存储椭圆模型的矩阵
-
-center = (round(113), round(155.6))
+center = (round(113), round(152))
 size = (25, 12)
-angle = 43
-# color = np.random.randint(0, high=256, size=(3, )).tolist()
+angle = -20
 color = 255
 thickness = np.random.randint(1, 9)
-img = cv2.ellipse(oval_matrix, center, size, angle, 0, 360, color, thickness=-1)
-cv2.imshow('name', oval_matrix)
+img = cv2.ellipse(oval_matrix, center, size, angle, 0, 360, color, thickness=-1)  # 取得椭圆肤色模型
+# img_csv = DataFrame(img)
+# img_csv.to_csv(img_csv_path, index=False, header=False, mode='w', decimal=',')
+# print(img)
+
+x, y, d = img_read.shape
+x_matrix = np.zeros((x, y), dtype='uint8')  # 建立一个匹配的二值矩阵图像
+Y, Cr, Cb = cv2.split(img_YCrCb)   # 取得三种矩阵
+# Cb_csv = DataFrame(Cb)
+# Cb_csv.to_csv(Cb_csv_path, index=False, header=False, mode='w', decimal=',')
+# 遍历图片的每一个像素点
+for row in range(x):
+    for col in range(y):
+        Cr_v = Cr[row, col]
+        Cb_v = Cb[row, col]
+        x_matrix[row, col] = img[Cb_v, Cr_v]
+
+x_csv = DataFrame(x_matrix)
+x_csv.to_csv(face_out_path, index=False, header=False, decimal=',', mode='w')
+# 将图片和矩阵进行与运算
+and_matrix = np.zeros((x, y, 3), dtype='uint8')
+and_matrix[:, :, 0] = x_matrix
+and_matrix[:, :, 1] = x_matrix
+and_matrix[:, :, 2] = x_matrix
+img_and = cv2.bitwise_and(img_read, and_matrix)
+# print(Y, Cr, Cb)
+cv2.imshow('name', img_and)
+# cv2.imwrite(oval_name, img)
 cv2.waitKey(0)
-
-'''
-def make_oval(cb, cv):
-    """
-    :param cb: 色彩空间Cb
-    :param cv:
-
-    :return:
-    """
-    cx = 109.38
-    cy = 152.02
-    angle = 2.53
-    ecx = 1.6
-    ecy = 2.41
-    a = 25.39
-    b = 14.03
-    x = math.cos(angle) * (cb - cx) + math.sin(angle) * (cv - cy)
-    y = -math.sin(angle) * (cb - cx) + math.cos(angle) * (cv - cy)
-    output = ((x - ecx)**2)/(a**2) + ((y - ecy)**2)/(b**2)
-    print(output)
-    if (output <= 1) and (output >= 0):
-        oval_matrix[cb, cv] = 1
-    else:
-        oval_matrix[cb, cv] = 0
-    # print(oval_matrix[cb, cv])
-    # time.sleep(0.01)
-
-
-# 循环建立椭圆肤色模型
-for row in range(5):
-    for col in range(5):
-        make_oval(row, col)
-
-oval_csv = DataFrame(oval_matrix)
-oval_csv.to_csv(file_path, index=False, header=False, mode='w')
-'''
-
